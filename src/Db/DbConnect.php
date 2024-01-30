10485719785e381ba38ba1562bff1fdcc89ecf32
Mdevoldere206 <mdevoldere@users.noreply.github.com>
2024-01-30 10:02:28 +0100

    Creation configuration

--- src/Db/DbConnect.php
+++ src/Db/DbConnect.php
@@ -2,6 +2,7 @@
 
 namespace Mdevoldere\PhpLibrary\Db;
 
+use Exception;
 use PDO; 
 
 /**
@@ -9,6 +10,8 @@ use PDO;
  */
 class DbConnect
 {
+
+    private static array $config;
     /**
      * Constructeur privé (la classe n'est pas instanciable)
      */
@@ -25,12 +28,25 @@ class DbConnect
     public static function getInstance(): PDO
     {
         if(self::$instance === null) {
-            self::$instance = new PDO('mysql:host=localhost;port=3306;dbname=db_cars;charset=utf8', 'root', '', [
+            self::$instance = new PDO(
+                'mysql:host='.self::$config['db_host'].';port='.self::$config['db_port'].';dbname='.self::$config['db_name'].';charset=utf8', 
+                self::$config['db_user'], 
+                self::$config['db_password'], [
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             ]);
         }
         return self::$instance;
+    }
+
+    public static function setConfiguration(string $chemin): void 
+    {
+        if(is_file($chemin)) {
+            self::$config = require $chemin;
+           // var_export(self::$config);
+        } else {
+            throw new Exception('Configuration base de données invalide');
+        }
     }
 }
